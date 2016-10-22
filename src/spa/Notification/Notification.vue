2f62752fa95492a71522d7819dc9b41185d64d36
eduardogc <eduardogc@outlook.com>
2016-10-22 17:42:06 -0200

    building notification

--- src/spa/Notification/Notification.vue
+++ src/spa/Notification/Notification.vue
@@ -1,32 +1,24 @@
 <template lang="html">
-  <div class="Notification__actions">
-    <!-- <navigation></navigation> -->
-    oi
+  <div class="">
+    <action-bar></action-bar>
   </div>
 </template>
 
 <script type="text/babel">
-// import Navigation from '../../shared-components/Navigation';
-// import Item from './Item';
+import ActionBar from '../../shared-components/ActionBar';
 
 export default {
   data() {
     return {};
   },
   computed: {},
-  components: {},
+  components: {
+    ActionBar,
+  },
   methods: {},
   mounted() {},
 };
 </script>
 
 <style lang="stylus">
-  @import "../../assets/styles/variables"
-  @import "../../assets/styles/mixins"
-
-  +prefix-classes('Notification__')
-    .actions
-      flex()
-      flex-justify(space-around)
-      padding 5vh 15px
 </style>
