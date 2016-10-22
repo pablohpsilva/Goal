a6a9ff0025b78790c5d8d15d53226ea7adfc9ec6
eduardogc <eduardogc@outlook.com>
2016-10-22 16:28:26 -0200

    building notifications page

---
+++ src/spa/Notification/Notification.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="Notification__actions">
+    <!-- <navigation></navigation> -->
+    oi
+  </div>
+</template>
+
+<script type="text/babel">
+// import Navigation from '../../shared-components/Navigation';
+// import Item from './Item';
+
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+  components: {},
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<style lang="stylus">
+  @import "../../assets/styles/variables"
+  @import "../../assets/styles/mixins"
+
+  +prefix-classes('Notification__')
+    .actions
+      flex()
+      flex-justify(space-around)
+      padding 5vh 15px
+</style>
