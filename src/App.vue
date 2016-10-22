a3a3abb11af8ac662ca23cb3c76f334d41fc8f84
Lucas Vilaboim <lucas@vilaboim.com>
2016-10-22 04:46:59 -0200

    Animate.css added

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,8 @@ export default {
 </script>
 
 <style lang="stylus">
+@import "assets/css/style.css"
+
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
