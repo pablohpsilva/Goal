1865fe81152fe40ba7fd31b79083b8a1e9efb344
pablohenrique <pablo.haoko@gmail.com>
2016-10-22 22:05:40 -0200

    chart

--- src/shared-components/Chart.vue
+++ src/shared-components/Chart.vue
@@ -1,79 +1,87 @@
+<style lang="less">
+</style>
+
 <template lang="html">
-  <canvas id="chart-{{uid}}"
-    v-bind.width="width"
-    v-bind.height="height"></canvas>
+    <div>
+        <canvas class="myChart" width="100" height="55" v-bind:id="uniqueID"></canvas>
+    </div>
 </template>
 
-<script>
+<script type="text/babel">
+import ChartJS from 'chart.js';
+
 export default {
   props: {
-    width: {
-      type: Number,
-      default() { return 400; },
+    type: {
+      type: String,
+      default() { return 'line'; },
     },
-    height: {
-      type: Number,
-      default() { return 400; },
+    chartData: {
+      type: Object,
+      default() { return {}; },
     },
-    data: {
-      type: Array,
-      default() { return [12, 19, 3, 5, 2, 3]; },
+    options: {
+      type: Object,
+      default() {
+        return {
+          scales: {
+            xAxes: [{
+              stacked: true,
+            }],
+            yAxes: [{
+              stacked: true,
+            }],
+          },
+          hover: {
+            mode: 'label',
+          },
+          animation: false,
+        };
+      },
     },
   },
   data() {
     return {
       chart: null,
-      uid: Math.ceil(Math.random() * Math.random() * 100),
     };
   },
-  computed: {},
-  components: {},
-  methods: {
-    createChart() {
-      document.getElementById("myChart")
-      this.chart = new Chart(
-        document.getElementById(`chart${this.uid}`),
-        {
-          type: 'bar',
-          data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-              label: '# of Votes',
-              data: this.data,
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-            }]
-          },
-          options: {
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true
-                }
-              }]
-            }
-          }
-        });
+  computed: {
+    uniqueID() {
+      return `chart-${Math.ceil(Math.random() * Math.random() * 1000)}`;
     },
   },
-  mounted() {},
+  watch: {
+    chartData() {
+      this.render();
+    },
+  },
+  methods: {
+    setChartGlobals() {
+      const hide = {
+        display: false,
+      };
+      ChartJS.defaults.global.title = hide;
+      ChartJS.defaults.global.legend = hide;
+      ChartJS.defaults.global.tooltips = { enabled: false };
+    },
+    render() {
+      /* eslint-disable no-undef */
+      const canvas = window.document.getElementById(this.uniqueID);
+      if (canvas && this.chartData) {
+        this.chart = new ChartJS(
+          canvas.getContext('2d'), {
+            type: this.type,
+            data: this.chartData,
+            options: this.options,
+          }
+        );
+        this.chart.generateLegend();
+      }
+    },
+  },
+  mounted() {
+    this.setChartGlobals();
+    this.render();
+  },
 };
 </script>
-
-<style lang="stylus">
-</style>
